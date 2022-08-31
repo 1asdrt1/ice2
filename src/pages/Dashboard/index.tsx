76c505a1bba1fe78bbdede5acb57d3540ebf5a97
李士林 <lsl358915905@gmail.com>
2022-09-01 00:26:09 +0800

    fix:更新

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-08-31 22:34:13
- * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-08-31 23:32:54
+ * @LastEditors: 李士林 lsl358915905@gmail.com
+ * @LastEditTime: 2022-09-01 00:26:04
  * @FilePath: \ice1\src\pages\Dashboard\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,7 +16,7 @@ const Dashboard = () => {
   return (
     <ResponsiveGrid gap={20}>
       <Cell colSpan={12}>
-        首页
+        首页1
       </Cell>
     </ResponsiveGrid>
   );
